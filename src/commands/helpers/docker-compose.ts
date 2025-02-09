dcbf78d3980f1119a22a913c2c0d3bbcc4cc11c6
thoroc <thomas.a.roche@gmail.com>
2025-02-09 14:44:34 +0000

    showing all available services when no running services exist

--- src/commands/helpers/docker-compose.ts
+++ src/commands/helpers/docker-compose.ts
@@ -1,14 +1,16 @@
 import { type Compose } from '@json-types/compose';
 import { parse } from 'jsr:@std/yaml';
 import * as dc from 'npm:docker-compose';
+import { DOCKER_COMPOSE_FILE } from './constants.ts';
 
-export const getServices = (filePath: string): string[] => {
-  const data = Deno.readTextFileSync(filePath);
+export const getServices = (filePath?: string): string[] => {
+  const dockerComposeFile = filePath || DOCKER_COMPOSE_FILE;
+  const data = Deno.readTextFileSync(dockerComposeFile);
   const values: Compose = parse(data) as Compose;
   const services = values.services;
 
   if (!services) {
-    throw new Error('No services found in docker-compose.yml');
+    throw new Error(`No services found in ${dockerComposeFile}`);
   }
 
   return Object.keys(services || {});
--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,12 +1,16 @@
 import { Command } from 'jsr:@cliffy/command@1.0.0-rc.7';
 import chalk from 'npm:chalk';
-import { getRunningServices } from './helpers/docker-compose.ts';
+import { getRunningServices, getServices } from './helpers/docker-compose.ts';
 
 const listAction = async () => {
   const runningServices = await getRunningServices();
 
   if (runningServices.length === 0) {
     console.log('No running services');
+    console.log('available services:');
+    for (const service of getServices()) {
+      console.log(`> ${chalk.yellow(service)}`);
+    }
     return;
   }
 
@@ -20,6 +24,7 @@ const listAction = async () => {
 const listCommand = new Command()
   .name('list')
   .description('Lists the applications')
+  .option('-A, --all', 'List all applications')
   .action(listAction);
 
 export { listCommand };

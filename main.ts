41b8d6a0775b21ea6f0ead5bf839351cd9199b46
thoroc <thomas.a.roche@gmail.com>
2025-02-09 15:50:40 +0000

    plsitting the docker-compose into separate files

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-import { Command } from "@cliffy/command";
-import { envCommand } from "./src/commands/env.ts";
-import { listCommand } from "./src/commands/list.ts";
-import { startCommand } from "./src/commands/start.ts";
-import { stopCommand } from "./src/commands/stop.ts";
+import { Command } from '@cliffy/command';
+import { envCommand } from './src/commands/env.ts';
+import { listCommand } from './src/commands/list.ts';
+import { startCommand } from './src/commands/start.ts';
+import { stopCommand } from './src/commands/stop.ts';
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   await new Command()
-    .name("cliffy")
-    .version("0.1.0")
-    .description("Command line framework for Deno")
-    .command("list", listCommand)
-    .command("start", startCommand)
-    .command("stop", stopCommand)
-    .command("env", envCommand)
+    .name('home-server-cli')
+    .version('0.1.0')
+    .description('CLI for managing home server')
+    .command('list', listCommand)
+    .command('start', startCommand)
+    .command('stop', stopCommand)
+    .command('env', envCommand)
     .parse(Deno.args);
 }
